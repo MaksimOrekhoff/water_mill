3cc797abb7ad36cfb525e072ffb40319f79a735b
Maksim Orekhov <maximorechov@yandex.ru>
2023-06-13 19:55:10 +0500

    1 added spring boot and mill resources

---
+++ mill/src/test/java/com/orekhov/mill/MillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orekhov.mill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mill_ui/pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-container style=" background: #2abd5b; text-align: center; justify-content: center;">
+    <b-row>
+      <b-col style="padding: 5%;">
+        <ul class="tools">
+          <li class="water">
+            <b-badge variant="primary">0</b-badge>
+          </li>
+          <li class="txt">+</li>
+          <li class="millet">
+            <b-badge variant="warning">0</b-badge>
+          </li>
+          <li class="txt">=</li>
+          <li class="flour">
+            <b-badge variant="primary">0</b-badge>
+          </li>
+        </ul>
+
+
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+.tools {
+  list-style: none;
+  display: inline-flex;
+}
+
+.tools li {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  border: #35495e 3px solid;
+}
+
+.tools li.txt {
+  border: none;
+  line-height: 80px;
+  color: #fff;
+  font-size: 200%;
+}
+
+.tools li.water {
+  background: #fff url(/water.png) no-repeat center center/cover;
+}
+
+.tools li.millet {
+  background: #fff url(/grain.png) no-repeat center center/cover;
+}
+
+.tools li.flour {
+  background: #fff url(/flour_PNG20.png) no-repeat center center/cover;
+}
+
+</style>
